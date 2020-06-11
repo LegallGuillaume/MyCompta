52f01dd326ae48b8ee1b960989bfe8bfac5efa5a
Le Gall Guillaume <gulegall13170@gmail.com>
2020-06-11 10:50:01 +0200

    add log to urls

--- website/urls/urls_client.py
+++ website/urls/urls_client.py
@@ -3,6 +3,7 @@ from models.client import Client, ClientDAO
 from models.color import Color
 from settings.tools import get_profile_from_session
 from flask import flash, request, render_template, redirect, session
+import logging
 
 __author__ = "Software Le Gall Guillaume"
 __copyright__ = "Copyright (C) 2020 Le Gall Guillaume"
@@ -27,15 +28,19 @@ def add_client(form):
     client.id_profile = profileSession.id
     cdao = ClientDAO()
     if cdao.insert(client):
+        logging.info('add client %s OK', client.name)
         flash('Le client {} a été ajoutée avec succès !'.format(client.name), 'success')
     else:
+        logging.warning('add client %s Failed', client.name)
         flash("Erreur lors de la création du client {} !".format(client.name), 'danger')
 
 def remove_client(clientname):
     cdao = ClientDAO()
     if cdao.delete(cdao.where('name', clientname)):
+        logging.info('remove client %s OK', clientname)
         flash('La facture {} a été supprimée avec succès !'.format(clientname), 'success')
     else:
+        logging.warning('remove client %s Failed', clientname)
         flash("Erreur lors de la suppression de la facture {} !".format(clientname), 'danger')
 
 def get_list_client(id_profile):
@@ -46,6 +51,7 @@ def get_list_client(id_profile):
 def cl():
     if not session.get('logged_in'):
         return redirect('/')
+    logging.info('go url /clients with ' + request.method)
     if request.method == 'GET':
         profile=get_profile_from_session()
         l_clients = get_list_client(profile.id)
@@ -61,5 +67,6 @@ def cl():
 def cl_del():
     if not session.get('logged_in'):
         return redirect('/')
+    logging.info('receive socket from /client-delete')
     remove_client(request.form['client-name'])
     return redirect('/clients')
